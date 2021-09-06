53c9fb97f79df03b248072ed9e322ad335473352
T.Prashanth <Prashanth.art2001@gmail.com>
2021-09-07 04:07:27 +0530

    config changed

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ const userApiRoute = require("./surveyApi/user");
 
 app.use("/user",userApiRoute);
 
-const port = 5000;
+const port = process.env.port||5000;
 app.listen(port,()=>console.log(`server on port ${port}`));
